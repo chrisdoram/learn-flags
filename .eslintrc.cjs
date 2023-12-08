7b81cc2c25bde19f9f04b14d539a085ef79e7ac5
Chris Oram <chrisoramdev@outlook.com>
2023-12-08 16:50:41 +0000

    Add prettier and configure eslint
    Add prettier config
    Configure eslint with strict typing and plugins
    Run lint and prettier on files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,31 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh', 'react', 'react-hooks', 'jsx-a11y', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error',
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 }
